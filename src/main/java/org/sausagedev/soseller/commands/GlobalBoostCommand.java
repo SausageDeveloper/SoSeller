a95547662988872ad50ccc021ee9633c37756253
SausageDev <mihailbtrnc@gmail.com>
2025-04-19 23:51:43 +0300

    Обновление 2.0.3, поддержка Java 16+

--- src/main/java/org/sausagedev/soseller/commands/GlobalBoostCommand.java
+++ src/main/java/org/sausagedev/soseller/commands/GlobalBoostCommand.java
@@ -7,21 +7,19 @@ import org.bukkit.entity.Player;
 import org.sausagedev.soseller.SoSeller;
 import org.sausagedev.soseller.configuration.Config;
 import org.sausagedev.soseller.configuration.data.MessagesField;
-import org.sausagedev.soseller.configuration.data.SettingsField;
 import org.sausagedev.soseller.utils.Utils;
 
 public class GlobalBoostCommand {
 
     public void execute(CommandSender sender, String[] args) {
         MessagesField messages = Config.messages();
-        SettingsField settings = Config.settings();
 
         if (Utils.isNotDouble(args[3])) {
             Utils.sendMSG(sender, messages.numberFormatError(), args[3]);
             return;
         }
         double n = Double.parseDouble(args[3]);
-        double globalBoost = settings.globalBoost();
+        double globalBoost = Config.settings().globalBoost();
         switch (args[2]) {
             case "set":
                 Config.setGlobalBoost(n);
@@ -34,7 +32,7 @@ public class GlobalBoostCommand {
                 Config.setGlobalBoost(res < 1 ? 1 : res);
                 break;
         }
-        globalBoost = settings.globalBoost();
+        globalBoost = Config.settings().globalBoost();
         String msg = messages.globalBoostModify();
         msg = msg.replace("{amount}", String.valueOf(globalBoost));
         if (sender instanceof Player && SoSeller.usePAPI()) msg = PlaceholderAPI.setPlaceholders((OfflinePlayer) sender, msg);
